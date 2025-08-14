8e4b842c5d458f2cf9a532baa69151f17000740a
Oskar <oskar@example.com>
2025-08-14 10:28:28 -0300

    Corregido: error de sintaxis en src/index.js

--- src/index.js
+++ src/index.js
@@ -3,12 +3,8 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-<<<<<<< HEAD
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-=======
-root.render(<App />);
->>>>>>> 3a097a3fcc82f2380d48f0c671b2829a65bec281
